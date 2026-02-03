7dee7b07138afbcc34ae9da3834bd91f301e70d8
ChoonXiang <yeohcx@outlook.com>
2026-02-03 15:10:13 +0000

    Remove hello-arc and transfer-funds directories, including all associated files and configurations.

--- transfer-funds/register-secret.js
+++
@@ -1,8 +0,0 @@
-import { registerEntitySecretCiphertext } from "@circle-fin/developer-controlled-wallets";
-import "dotenv/config";
-
-const response = await registerEntitySecretCiphertext({
-  apiKey: process.env.CIRCLE_API_KEY,
-  entitySecret: process.env.CIRCLE_ENTITY_SECRET,
-  recoveryFileDownloadPath: "./", // SAVE THIS FILE!
-});
