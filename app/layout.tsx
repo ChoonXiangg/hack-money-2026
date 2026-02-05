35013344a15815260985cf9e67cf11fdb857b236
ChoonXiang <yeohcx@outlook.com>
2026-02-05 22:42:20 +0000

    Created upload song page with local storage

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,19 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist_Mono, Climate_Crisis, Murecho } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const murecho = Murecho({
+  variable: "--font-murecho",
   subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+const climateCrisis = Climate_Crisis({
+  variable: "--font-climate",
   subsets: ["latin"],
 });
 
@@ -25,7 +30,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${murecho.variable} ${geistMono.variable} ${climateCrisis.variable} font-[family-name:var(--font-murecho)] antialiased`}
       >
         {children}
       </body>
