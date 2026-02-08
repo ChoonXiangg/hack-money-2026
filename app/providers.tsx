c777fe7f2f87dc81b0788e1282ae090187476fe1
Yi Zheng Tang <yzhengtang4@gmail.com>
2026-02-08 12:36:04 +0000

    Changed background design

--- app/providers.tsx
+++ app/providers.tsx
@@ -3,19 +3,31 @@
 import { HeroUIProvider } from "@heroui/react";
 import { TransactionProvider } from "@/context/TransactionContext";
 import TransactionToast from "@/components/TransactionToast";
-import Squares from "@/components/Squares";
+import PixelBlast from "@/components/PixelBlast";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <HeroUIProvider>
       <TransactionProvider>
         <div className="fixed inset-0 z-0">
-          <Squares
-            speed={0.29}
-            squareSize={36}
-            direction="left"
-            borderColor="#865d8e"
-            hoverFillColor="#b08787"
+          <PixelBlast
+            variant="square"
+            pixelSize={3}
+            color="#B19EEF"
+            patternScale={4.25}
+            patternDensity={1.3}
+            pixelSizeJitter={0.6}
+            enableRipples
+            rippleSpeed={0.4}
+            rippleThickness={0.12}
+            rippleIntensityScale={1.5}
+            liquid={false}
+            liquidStrength={0.12}
+            liquidRadius={1.2}
+            liquidWobbleSpeed={5}
+            speed={0.5}
+            edgeFade={0.25}
+            transparent
           />
         </div>
         {children}
